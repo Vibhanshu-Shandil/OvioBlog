4ba70d4a1216cd11fa052a451d541cf3b068b7fa
Vibhanshu Shandil <vibhanshushandil31@gmail.com>
2020-08-22 09:09:02 +0530

    Get all user and user by id route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator/check');
+const checkObjectId = require('../../middleware/checkObjectId');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -94,5 +95,41 @@ router.post(
       
     }
 );
+
+// @route    GET api/profile
+// @desc     Get all profiles
+// @access   Public
+router.get('/', async (req, res) => {
+    try {
+      const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+      res.json(profiles);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  });
+
+// @route    GET api/profile/user/:user_id
+// @desc     Get profile by user ID
+// @access   Public
+router.get(
+    '/user/:user_id',
+    checkObjectId('user_id'),
+    async ({ params: { user_id } }, res) => {
+      try {
+        const profile = await Profile.findOne({
+          user: user_id
+        }).populate('user', ['name', 'avatar']);
+  
+        if (!profile) return res.status(400).json({ msg: 'Profile not found' });
+  
+        return res.json(profile);
+      } catch (err) {
+        console.error(err.message);
+        return res.status(500).json({ msg: 'Server error' });
+      }
+    }
+  );
+
 
 module.exports = router;
