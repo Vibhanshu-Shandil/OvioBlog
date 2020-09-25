ccbc56106ad7dedd1305498ce401e4b3d52417d1
Vibhanshu Shandil <vibhanshushandil31@gmail.com>
2020-09-25 21:48:54 +0530

    Added Profile component

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,4 @@
-import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE, UPDATE_PROFILE, GET_PROFILES, GET_REPOS } from '../actions/types';
+import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE, UPDATE_PROFILE, GET_PROFILES, GET_REPOS, NO_REPOS } from '../actions/types';
 
 const initialState = {
     profile: null,
@@ -37,13 +37,18 @@ export default function (state = initialState, action) {
                 profile: null,
                 repos: [],
                 loading: false
-            }
+            };
         case GET_REPOS:
             return {
                 ...state,
                 repos: payload,
                 loading: false
-            }
+            };
+        case NO_REPOS:
+            return {
+                ...state,
+                repos: []
+            };
         default:
             return state;
     }
